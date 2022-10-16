f6163bb53d6f7bea70055207608288f9c4e64da0
SergieAGA2 <sergiearruda@gmail.com>
2022-10-16 16:55:59 +0100

    Basic components finished

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Text, TextProps } from "./Text";
+import { Heading, HeadingProps } from "./Heading";
 
 export default {
-  title: "Component/Text",
-  component: Text,
+  title: "Component/Heading",
+  component: Heading,
   args: {
     children: "Lorem Ipsum",
     size: "md",
@@ -16,23 +16,23 @@ export default {
       },
     },
   },
-} as Meta<TextProps>;
+} as Meta<HeadingProps>;
 
-export const Default: StoryObj<TextProps> = {};
-export const Small: StoryObj<TextProps> = {
+export const Default: StoryObj<HeadingProps> = {};
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: "sm",
   },
 };
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: "lg",
   },
 };
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
-    children: <p>Test with a P tag</p>,
+    children: <h1>Heading with a H1 tag</h1>,
   },
   argTypes: {
     children: {
